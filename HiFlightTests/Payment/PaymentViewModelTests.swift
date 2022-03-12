134dd906ca229d009db0a979f54225defb352656
Shuai Wang <shuai.wang@thoughtworks.com>
2022-03-13 06:34:46 +0800

    [Story 1] #AC2 工序2: sub Networking, test whether payment's viewmodel could handle balance not enough case.

--- HiFlightTests/Payment/PaymentViewModelTests.swift
+++ HiFlightTests/Payment/PaymentViewModelTests.swift
@@ -30,6 +30,23 @@ class PaymentViewModelTests: QuickSpec {
                     }
                     expect(expected).to(equal(actual))
                 }
+                
+                it("shoudle callback failed logic in viewmodel when payment is failed because of balance isn't enough") {
+                    let orderId = 888888
+                    let payType = PayType.balance
+                    
+                    let subModel = PaymentModel(code: .notEnough, message: "余额不足")
+                    let expected = PaymentResultModel(code: "balance_not_enough", message: "余额不足")
+                    var actual: PaymentResultModel?
+                    
+                    let viewmodel = PaymentViewModel()
+                    viewmodel._setSubTestModel(model: subModel)
+                    viewmodel.payWithOrderId(orderId: orderId, payType: payType) { result in
+                        actual = result
+                    } failure: { error in
+                    }
+                    expect(expected).to(equal(actual))
+                }
             }
         }
     }
