52fa423d9de6ac4c828c7dde9fa176ee2ddcf80a
Shuai Wang <shuai.wang@thoughtworks.com>
2022-03-13 12:54:42 +0800

    [Story 2] #AC1 工序1: add invoice apply viewControlller and apply result view.

---
+++ HiFlight/View/InvoiceController.swift
@@ -0,0 +1,19 @@
+//
+//  InvoiceController.swift
+//  HiFlight
+//
+//  Created by shuai.wang on 2022/3/13.
+//
+
+import UIKit
+
+class InvoiceController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+
+    @IBAction func apply(_ sender: Any) {
+        
+    }
+    
+}
